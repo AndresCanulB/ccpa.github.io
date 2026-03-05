52a3f21957dac2c704ce7a0747f2d7d8bfe64420
AndresCanulB <acanulb70494@gmail.com>
2026-03-04 18:31:10 -0600

    Actualizacion

---
+++ js/cards.js
@@ -0,0 +1,31 @@
+function getVmin(value) {
+  return (Math.min(window.innerWidth, window.innerHeight) / 100) * value;
+}
+
+new Swiper(".main__swiper", {
+  wrapperClass: 'swiper__wrapper',
+  slideClass: 'swiper__slide',
+  spaceBetween: getVmin(5),
+  speed: 5000,
+  loop: true,
+  allowTouchMove: false,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: getVmin(100),
+    },
+    700: {
+      slidesPerView: 2,
+      spaceBetween: getVmin(5),
+    },
+    1100: {
+      slidesPerView: 4,
+      spaceBetween: getVmin(5),
+    }
+  }
+});
