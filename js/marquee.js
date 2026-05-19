fa81f24d81874cb2ea490b339bf92585a674a854
AndresCanulB <acanulb70494@gmail.com>
2026-05-19 10:54:23 -0600

    Actualizacion

--- js/marquee.js
+++
@@ -1,8 +0,0 @@
-
-let marquee = document.querySelector(".main__marquee");
-let group = document.querySelector(".marquee__group");
-let copy = group.cloneNode(true);
-
-copy.setAttribute("aria-hidden", "true");
-
-marquee.appendChild(copy);
